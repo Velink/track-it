c8d9c6aae25ae0eca9fc80303132caa7390fcf81
mussb00 <berhanem2000@gmail.com>
2021-10-02 18:29:53 +0100

    added more methods to the user class.User can now access all habits and create a habit

--- api/models/user.js
+++ api/models/user.js
@@ -1,14 +1,15 @@
 const { init } = require ('../dbConfig')
 const { ObjectId } = require('mongodb')
 
+const Habit = require('./habit')
+
 class User {
     constructor(data){
-        this.id = data.id
         this.name = data.name
-        this.hash = data.hash
+        this.hash = data.hash // hash == hashed password
     }
 
-    // may not need this
+    // grab all users. may not need this
     static get all() {
         return new Promise (async (resolve, reject) => {
             try {
@@ -24,7 +25,7 @@ class User {
         })
     }
 
-    
+    // grab single user
     static findById (id) {
         return new Promise (async (resolve, reject) => {
             try {
@@ -43,8 +44,37 @@ class User {
         return new Promise (async (resolve, reject) => {
             try {
                 const db = await init();
-                let userData = await db.collection('users').insertOne({ name, hash })
-                let newUser = new Dog(userData.ops[0]); // .rows
+                let userData = await db.collection('users').insertOne({ name: name, hash : hash }) 
+                let newUser = new User(userData.ops[0]); // .rows
+                resolve (newUser);
+            } catch (err) {
+                reject('Error creating user');
+            }
+        });
+    }
+
+    get habits(userId){
+        return new Promise (async (resolve, reject) => {
+            try {
+                const db = await init();
+                const user = await db.collections('users').find({_id: ObjectId(userId)}); //mongo stores id as object
+                const userHabits = user["habits"]; // user['habits'] should be stored in db as array we can push objects to
+                resolve(userHabits);
+            } catch (err) {
+                reject("Users habits could not be found");
+            };
+        });
+    };
+
+    //creates habit for single user
+    static createHabit(userId, name, frequency){
+        return new Promise (async (resolve, reject) => {
+            try {
+                const db = await init();
+                const user = await db.collections('users').find({_id: ObjectId(userId)});
+                const userHabitsData = user["habits"];
+                userHabitsData.push({name : name , frequency: frequency})
+                let newUser = new User(userData.ops[0]); // .rows
                 resolve (newUser);
             } catch (err) {
                 reject('Error creating user');
