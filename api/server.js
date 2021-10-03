698a5f9dd62a008249512c7787e7f7cd074e6e2f
MathusanKandiah <mathusankandiah@hotmail.com>
2021-10-03 18:29:37 +0100

    registration route complete and will store user_id, username, and unhashed password (can be changed to hashed but kept as unhashed for development purposes)

--- api/server.js
+++ api/server.js
@@ -1,14 +1,35 @@
 const express = require('express');
+const bcrypt = require('bcryptjs');
 const cors = require('cors');
+const User = require('./models/user')
 
 const server = express();
 server.use(cors());
 server.use(express.json());
+
 
 const userRoutes = require('./controllers/userRoutes')
 server.use('/users', userRoutes)
 
 // Root route
 server.get('/', (req, res) => res.send('Hello, client!'))
+
+// registration route
+server.post('/register', async (req, res) => { 
+    try {
+        console.log(req.body.password)
+        const salt = await bcrypt.genSalt(); 
+        const hashed = await bcrypt.hash(req.body.password, salt); 
+        const username = req.body.username
+        password = req.body.password
+        console.log(username)
+        console.log(req.body.password)
+        console.log(hashed)
+        await User.create(username,password); //feeding in the unhashed password for checking if database stored value is correct (just need to replace with 'hashed' instead)
+        res.status(201).json({msg: 'User created'});
+    } catch (err) {
+        res.status(500).json({err});
+    }
+})
 
 module.exports = server
