94bc2002006a1a4800ac755708a94cc4f5f4d621
mussb00 <berhanem2000@gmail.com>
2021-10-04 10:39:03 +0100

    changed secret to env variable

--- api/tokenAuth.js
+++ api/tokenAuth.js
@@ -4,7 +4,7 @@ function authenticateToken(req, res, next){
     const token = authHeader && authHeader.split(' ')[1] //if authHeader not null return the token
     if (token == null) return res.sendStatus(401)
     // verifies the token using sectret token and returns user info if match
-    jwt.verify(token, 'supersecret-secret', async (err, data) => {
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, async (err, data) => {
         if(err){
             res.status(403).json({ err: 'Invalid token' })
         } else {
