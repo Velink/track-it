48041ba8e22854913723273fe4e2797c5d93ea9c
MathusanKandiah <mathusankandiah@hotmail.com>
2021-10-05 18:19:54 +0100

    working on sending register to create user

--- client/scripts/requests.js
+++ client/scripts/requests.js
@@ -1,28 +1,36 @@
 function submitData(e){
     e.preventDefault()
-    habitData = []
-    let habits = document.getElementsByTagName('input')
-    for (let habit of habits){
-      let habitList = {habit: habit.value}
-      habitData.push(habitList)
-    }
-    let frequencySelectors = document.getElementsByTagName('select');
-    console.log(frequencySelectors)
-    for(let i = 0 ; i < frequencySelectors.length ; i++){
-      let newFreq = frequencySelectors[i].options.selectedIndex
-      habitData[i].frequency = newFreq
-    }
-    console.log(habitData) // returns an array of format = [{habit: <habitname>, frequency: <frequency>}]
+    // habitData = []
+    // let habits = document.getElementsByTagName('input')
+    // for (let habit of habits){
+    //   let habitList = {habitName: habit.value}
+    //   habitData.push(habitList)
+    // }
+    // let frequencySelectors = document.getElementsByTagName('select');
+    // console.log(frequencySelectors)
+    // for(let i = 0 ; i < frequencySelectors.length ; i++){
+    //   let newFreq = frequencySelectors[i].options.selectedIndex
+    //   habitData[i].frequency = newFreq
+    // }
+    // console.log(habitData) // returns an array of format = [{habit: <habitname>, frequency: <frequency>}]
   
     //get email first and then add that to the habitsdata
+
+    let habit = document.getElementById('initHabit').value
+    // console.log(habit)
+    let frequency = document.getElementsByTagName('select')[0].value
+    // console.log(frequency)
+    let userEmail = localStorage.getItem("userEmail")
+    let postingData = {email: userEmail, habitName: habit, frequency:frequency}
+    // console.log(postingData)
   
-    // const options = {
-    //   method: 'PATCH',
-    //   headers: {
-    //       'Content-Type': 'application/json'
-    //   },
-    //   body: JSON.stringify(habitData) // sends data of all chosen habits in the form of =>    habitData = [{habit: chosenhabit, frequency: chosen frequency}]
-    // }
-    // fetch('http://localhost:3000', options) // choose where to send it to
+    const options = {
+      method: 'PATCH',
+      headers: {
+          'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(postingData) // sends data of all chosen habits in the form of =>    habitData = [{habit: chosenhabit, frequency: chosen frequency}]
+    }
+    fetch(`http://localhost:3000/${userEmail}/choose_habits`, options) // choose where to send it to
     
   }
